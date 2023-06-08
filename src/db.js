41a03aa9f1ebb4bf46250aab21cede9d5d86ff9b
davif13 <davi.ffa@gmail.com>
2023-06-07 21:52:08 -0300

    vercel configs

--- src/db.js
+++ src/db.js
@@ -1,33 +1,8 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-const {
-  POSTGRESFIN_USER,
-  POSTGRESFIN_PASSWORD,
-  POSTGRESFIN_DATABASE,
-  POSTGRESFIN_HOST,
-  DB_PORT,
-  POSTGRESFIN_URL,
-} = process.env;
-
-const db = new Pool(
-  DB_URL
-    ? {
-        connectionString: POSTGRESFIN_URL,
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      }
-    : {
-        user: POSTGRESFIN_USER,
-        password: POSTGRESFIN_PASSWORD,
-        database: POSTGRESFIN_DATABASE,
-        host: POSTGRESFIN_HOST,
-        port: Number(DB_PORT),
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      }
-);
+const db = new Pool({
+  connectionString: process.env.POSTGRESFIN_URL + "?sslmode=require",
+});
 
 module.exports = db;
